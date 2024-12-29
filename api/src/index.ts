40ace49f3bcf0def21ed9ce49a283ac6529ecf6c
sahil <sahilabdulalim900@gmail.com>
2024-12-29 14:18:02 +0300

    Products routes setup

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,13 +1,16 @@
 import express from 'express';
+import productsRoutes from './routes/products/index';
+
+const app = express();
 
 const port = 3000;
 
-const app = express()
-
 app.get('/', (req, res) => {
-    res.send("Hello World")
+    res.send("Hello World!");
 })
 
+app.use('/products', productsRoutes);
+
 app.listen(port, () => {
-    console.log(`Example listening on port ${port}`)
+    console.log(`App listening at port ${port}`);
 })
